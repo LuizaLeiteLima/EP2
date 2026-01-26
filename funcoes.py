23b510554af9490c64f089d98624650d47a655bc
Luiza <luizall3@al.insper.edu.br>
2026-01-26 12:17:22 -0300

    certo

--- funcoes.py
+++ funcoes.py
@@ -23,19 +23,33 @@ def inicia_jogo (jogadores,tabuleiro):
     
     pecas = 7
 
-    for jogador in range(0,jogadores):
-        for lista in tabuleiro:
-            while pecas > len(resto['jogadores'][jogador]):
-                if jogador not in resto['jogadores']:
-                    resto['jogadores'][jogador] = lista
-                    tabuleiro.remove(lista)
-                else:
-                    resto['jogadores'][jogador].append(lista)
-                    tabuleiro.remove(lista)
+    for jogador in range(0, jogadores):
+        if jogador not in resto['jogadores']:
+            resto['jogadores'][jogador] = []
+
+    for lista in tabuleiro[:]:
+        if pecas > len(resto['jogadores'][jogador]):
+            resto['jogadores'][jogador].append(lista)
+            tabuleiro.remove(lista)
                 
     resto['monte'] = tabuleiro
     return resto
 
+
+
+jogadores = 2
+
+tabuleiro = [
+    [1,3],[0,1],[4,6],[0,3],[0,4],[6,6],[0,6],
+    [1,1],[1,2],[0,0],[1,4],[1,5],[1,6],[2,2],
+    [3,6],[2,4],[2,5],[2,6],[3,3],[3,4],[2,3],
+    [3,5],[4,4],[4,5],[0,2],[5,5],[5,6],[0,5]
+]
+
+
+resultado = inicia_jogo(jogadores, tabuleiro)
+
+print(resultado)
 
 
 
