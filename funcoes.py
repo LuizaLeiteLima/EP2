8d3d746ad0dfde136727f4f3dd3859cd129f6f63
Luiza <luizall3@al.insper.edu.br>
2026-01-26 11:42:33 -0300

    arru

--- funcoes.py
+++ funcoes.py
@@ -8,7 +8,7 @@ def cria_pecas ():
  
     for numero in range(0,7):
         for numero2 in range(0,7):
-            if [numero,numero2] or [numero2,numero] not in pecas:
+            if [numero,numero2] not in pecas and [numero2,numero] not in pecas:
                 pecas.append([numero,numero2])
 
     shuffle(pecas)
