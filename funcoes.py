95eff7406d4a5f20530b2cf29cfdd587244ea323
CarolPini44 <carolpini10@gmail.com>
2026-01-27 09:50:20 -0300

    adiciona_na_mesa

--- funcoes.py
+++ funcoes.py
@@ -64,3 +64,29 @@ def posicoes_possiveis(estado_mesa,lista_pecas):
             if lista_pecas[i][0]==dir or lista_pecas[i][1]==dir or lista_pecas[i][0]==esq or lista_pecas[i][1]==esq:
                 lista_ind.append(i)
     return lista_ind
+
+def adiciona_na_mesa(peca_col,lista_pecas):
+    saida = []
+    if len(lista_pecas)==0:
+        return saida.append(peca_col)
+    
+    esq = lista_pecas[0][0]
+    dir = lista_pecas[-1][1]
+    esq_peca = peca_col[0]
+    dir_peca = peca_col[1]
+
+
+    if esq_peca == lista_pecas[0][0]:
+        peca_col = peca_col[::-1]
+        saida = [peca_col, *lista_pecas]
+    elif esq_peca == lista_pecas[-1][1]:
+        saida = lista_pecas
+        saida = saida.append(peca_col)
+    elif dir_peca == lista_pecas[0][0]:
+        saida = [peca_col, *lista_pecas]
+    elif dir_peca == lista_pecas[-1][1]:
+        peca_col = peca_col[::-1]
+        saida = lista_pecas
+        saida = saida.append(peca_col)
+    return saida
+
