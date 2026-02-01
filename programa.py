2df251a47137121926b83ea1a0c14e003f5e02b6
CarolPini44 <carolinapq1@al.insper.edu.br>
2026-02-01 14:33:24 -0300

    arrumando

--- programa.py
+++ programa.py
@@ -20,18 +20,18 @@ while jogar == "sim":
     trava_jogo = 0 
     
    
-while vencedor == -1 and trava_jogo < quantidade_jogadores:
-        
-        print("\n--- VEZ DO JOGADOR", quem_joga, "---")
-        print("Mesa atual:", mesa)
-        
-        minhas_pecas = lista_jogadores[quem_joga]
-        
-        opcoes = posicoes_possiveis(mesa, minhas_pecas)
-        
-        while len(opcoes) == 0 and len(monte) > 0:
-            print("Voce nao tem peca! Pegando uma no monte...")
-            peca_nova = monte[0]
-            minhas_pecas.append(peca_nova)
-            monte.remove(peca_nova)
+    while vencedor == -1 and trava_jogo < quantidade_jogadores:
+            
+            print("\n--- VEZ DO JOGADOR", quem_joga, "---")
+            print("Mesa atual:", mesa)
+            
+            minhas_pecas = lista_jogadores[quem_joga]
+            
             opcoes = posicoes_possiveis(mesa, minhas_pecas)
+            
+            while len(opcoes) == 0 and len(monte) > 0:
+                print("Voce nao tem peca! Pegando uma no monte...")
+                peca_nova = monte[0]
+                minhas_pecas.append(peca_nova)
+                monte.remove(peca_nova)
+                opcoes = posicoes_possiveis(mesa, minhas_pecas)
