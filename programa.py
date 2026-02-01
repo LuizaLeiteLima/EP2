83db99ccf9a22062f36f1dfccd13e52f0699053e
CarolPini44 <carolinapq1@al.insper.edu.br>
2026-02-01 14:37:04 -0300

    valida pecas

--- programa.py
+++ programa.py
@@ -22,7 +22,7 @@ while jogar == "sim":
    
     while vencedor == -1 and trava_jogo < quantidade_jogadores:
             
-            print("\n--- VEZ DO JOGADOR", quem_joga, "---")
+            print("\nvez do jogador", quem_joga, "")
             print("Mesa atual:", mesa)
             
             minhas_pecas = lista_jogadores[quem_joga]
@@ -30,8 +30,25 @@ while jogar == "sim":
             opcoes = posicoes_possiveis(mesa, minhas_pecas)
             
             while len(opcoes) == 0 and len(monte) > 0:
-                print("Voce nao tem peca! Pegando uma no monte...")
+                print("nao tem peca, pegue uma do monte")
                 peca_nova = monte[0]
                 minhas_pecas.append(peca_nova)
                 monte.remove(peca_nova)
                 opcoes = posicoes_possiveis(mesa, minhas_pecas)
+
+    if len(opcoes) > 0:
+            trava_jogo = 0
+            
+            if quem_joga == 0:
+                print("suas pecas sao:", minhas_pecas)
+                print("indices das pecas que voce pode jogar:", opcoes)
+                
+                escolha = int(input("qual o indice da peca que voce quer jogar? "))
+                
+                while escolha not in opcoes:
+                    print("essa peca nao serve na mesa")
+                    escolha = int(input("escolha um indice valido: "))
+                
+                peca_escolhida = minhas_pecas[escolha]
+                minhas_pecas.remove(peca_escolhida)
+                mesa = adiciona_na_mesa(peca_escolhida, mesa)
